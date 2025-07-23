34a089817883340b4fc7a7a41cdbf4b2ece54242
DFD <darielfd45@gmail.com>
2025-07-23 10:14:00 +0200

    Building Insurances

--- src/app/meet-us/meet-us.ts
+++ src/app/meet-us/meet-us.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { SectionInfo } from "../section-info/section-info";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-meet-us',
-  imports: [],
+  imports: [RouterModule, SectionInfo, CommonModule],
   templateUrl: './meet-us.html',
   styleUrl: './meet-us.css'
 })
 export class MeetUs {
 
+  sectionInfo:boolean = false;
+
+  showSectionInfo(arg0:boolean){
+    this.sectionInfo = arg0;
+  }
 }
---
+++ src/app/section-info/section-info.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-section-info',
+  imports: [],
+  templateUrl: './section-info.html',
+  styleUrl: './section-info.css'
+})
+export class SectionInfo {
+
+}
