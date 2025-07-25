77229202ec664fd5ff93e6aea129c7d3c1428a06
DFD <darielfd45@gmail.com>
2025-07-25 11:17:54 +0200

    Fundación Completado excepto Pago

---
+++ src/app/charity-component/charity-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-charity-component',
+  imports: [],
+  templateUrl: './charity-component.html',
+  styleUrl: './charity-component.css'
+})
+export class CharityComponent {
+
+}
--- src/app/meet-us/meet-us.ts
+++ src/app/meet-us/meet-us.ts
@@ -2,18 +2,19 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { SectionInfo } from "../section-info/section-info";
 import { CommonModule } from '@angular/common';
+import { CharityComponent } from "../charity-component/charity-component";
 
 @Component({
   selector: 'app-meet-us',
-  imports: [RouterModule, SectionInfo, CommonModule],
+  imports: [RouterModule, SectionInfo, CommonModule, CharityComponent],
   templateUrl: './meet-us.html',
   styleUrl: './meet-us.css'
 })
 export class MeetUs {
 
-  sectionInfo:boolean = false;
+  sectionInfo:string = "";
 
-  showSectionInfo(arg0:boolean){
+  showSectionInfo(arg0:string){
     this.sectionInfo = arg0;
   }
 }
